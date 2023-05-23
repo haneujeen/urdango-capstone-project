50881dc8b062647380a70812d070b25b4750257f
haneujeen <eujeenhan@gmail.com>
2023-05-23 18:23:10 +0900

    Add models as follows: BusStop, SubwayStation, BusLine, SubwayLine, BusRouteStop, SubwayRouteStation, Bus, and Subway in the transit app

--- sources/urdango/transit/admin.py
+++ sources/urdango/transit/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import BusStop, SubwayStation, BusLine, SubwayLine, BusRouteStop, SubwayRouteStation, Bus, Subway
 
 # Register your models here.
+
+admin.site.register(BusStop)
+admin.site.register(SubwayStation)
+admin.site.register(BusLine)
+admin.site.register(SubwayLine)
+admin.site.register(BusRouteStop)
+admin.site.register(SubwayRouteStation)
+admin.site.register(Bus)
+admin.site.register(Subway)
--- sources/urdango/transit/models.py
+++ sources/urdango/transit/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
+
 # Create your models here.
+class BusStop(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+
+class SubwayStation(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+
+class BusLine(models.Model):
+    number = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+
+
+class SubwayLine(models.Model):
+    number = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    route_stations = models.ManyToManyField(SubwayStation, through='SubwayRouteStation')
+
+
+class BusRouteStop(models.Model):
+    bus_line = models.ForeignKey(BusLine, on_delete=models.CASCADE)
+    bus_stop = models.ForeignKey(BusStop, on_delete=models.CASCADE)
+    arrival_time = models.TimeField()
+    departure_time = models.TimeField()
+    sequence = models.IntegerField()
+
+
+"""
+might be represented like:
+    BusRouteStop.objects.create(bus_line=my_bus_line, bus_stop=A, sequence=1)
+    BusRouteStop.objects.create(bus_line=my_bus_line, bus_stop=B, sequence=2)
+    BusRouteStop.objects.create(bus_line=my_bus_line, bus_stop=C, sequence=3)
+"""
+
+
+class SubwayRouteStation(models.Model):
+    subway_line = models.ForeignKey(SubwayLine, on_delete=models.CASCADE)
+    subway_station = models.ForeignKey(SubwayStation, on_delete=models.CASCADE)
+    arrival_time = models.TimeField()
+    departure_time = models.TimeField()
+    sequence = models.IntegerField()
+
+
+class Bus(models.Model):
+    bus_identifier = models.CharField(max_length=200)
+    bus_line = models.ForeignKey(BusLine, on_delete=models.CASCADE)
+    current_stop = models.ForeignKey(BusStop, on_delete=models.CASCADE)
+    next_stop = models.ForeignKey(BusStop, related_name='+', on_delete=models.CASCADE)
+    estimated_arrival_time_next_stop = models.TimeField(null=True)
+
+
+class Subway(models.Model):
+    train_identifier = models.CharField(max_length=200)
+    subway_line = models.ForeignKey(SubwayLine, on_delete=models.CASCADE)
+    current_station = models.ForeignKey(SubwayStation, on_delete=models.CASCADE)
+    next_station = models.ForeignKey(SubwayStation, related_name='+', on_delete=models.CASCADE)
+    estimated_arrival_time_next_stop = models.TimeField(null=True)
