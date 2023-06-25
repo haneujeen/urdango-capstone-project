39f077b2471c8f2f043a7e27332b789684e0e8bd
haneujeen <eujeenhan@gmail.com>
2023-06-25 12:10:23 +0900

    Add SearchStation.vue

--- sources/client/src/App.vue
+++ sources/client/src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchStation from './components/SearchStation.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/logo.png" class="logo vue" alt="Vue logo" />
+        </a>
+        <SearchStation msg="" />
+    </div>
 </template>
 
 <style scoped>
