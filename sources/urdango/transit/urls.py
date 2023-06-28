8e4f157aa0ed2c290ba74fd819275698b3cc5544
haneujeen <eujeenhan@gmail.com>
2023-06-28 17:56:22 +0900

    Add API views

--- sources/urdango/transit/urls.py
+++ sources/urdango/transit/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BusDataViewSet
+from .views import BusDataViewSet, get_station_by_name, get_station_by_uid, get_bus_pos_by_rtid, \
+    get_arr_info_by_route_all, get_bus_pos_by_veh_id
 
 router = DefaultRouter()
 router.register(r'busdata', BusDataViewSet, basename='busdata')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('get_station_by_name/<str:name>/', get_station_by_name, name='get_station_by_name'),
+    path('get_station_by_uid/<str:id>/', get_station_by_uid, name='get_station_by_uid'),
+    path('get_bus_pos_by_rtid/<str:rtid>/', get_bus_pos_by_rtid, name='get_bus_pos_by_rtid'),
+    path('get_arr_info_by_route_all/<str:rtid>/', get_arr_info_by_route_all, name='get_arr_info_by_route_all'),
+    path('get_bus_pos_by_veh_id/<str:veh_id>/', get_bus_pos_by_veh_id, name='get_bus_pos_by_veh_id'),
 ]
--- sources/urdango/transit/views.py
+++ sources/urdango/transit/views.py
@@ -6,3 +6,69 @@ class BusDataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Bus.objects.all()
     serializer_class = BusDataSerializer
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import requests
+from urllib.parse import unquote
+
+@api_view(['GET'])
+def get_station_by_name(request, name):
+    url = 'http://ws.bus.go.kr/api/rest/stationinfo/getStationByName'
+    print(name)
+
+    params = {
+        'serviceKey': unquote('GMDNZxLlo35v0mYu1b%2BEExd5aIdZ93RCUBhUBo2w73LWCtz%2Ft%2F%2FKdGfzDVUdcyqljjwvNa5Dtd56uELhovFZRw%3D%3D'),
+        'stSrch': name,
+        'resultType': 'json'
+    }
+    response = requests.get(url, params=params)
+    return Response(response.json())
+
+
+@api_view(['GET'])
+def get_station_by_uid(request, id):
+    url = 'http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid'
+
+    params = {
+        'serviceKey': unquote('GMDNZxLlo35v0mYu1b%2BEExd5aIdZ93RCUBhUBo2w73LWCtz%2Ft%2F%2FKdGfzDVUdcyqljjwvNa5Dtd56uELhovFZRw%3D%3D'),
+        'arsId': id,
+        'resultType': 'json'
+    }
+    response = requests.get(url, params=params)
+    return Response(response.json())
+
+@api_view(['GET'])
+def get_bus_pos_by_rtid(request, rtid):
+    url = 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid'
+
+    params = {
+        'serviceKey': unquote('GMDNZxLlo35v0mYu1b%2BEExd5aIdZ93RCUBhUBo2w73LWCtz%2Ft%2F%2FKdGfzDVUdcyqljjwvNa5Dtd56uELhovFZRw%3D%3D'),
+        'busRouteId': rtid,
+        'resultType': 'json'
+    }
+    response = requests.get(url, params=params)
+    return Response(response.json())
+
+@api_view(['GET'])
+def get_arr_info_by_route_all(reqeust, rtid):
+    url = 'http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll'
+
+    params = {
+        'serviceKey': unquote('7Sd1p2xp2TxP2FZyZWaNvt%2FjsEW9ULgUgFhwXwMfcTW0vE59yvganCzAwTaCMg7p7todCjZq47xmZ90Av7GC7g%3D%3D'),
+        'busRouteId': rtid,
+        'resultType': 'json'
+    }
+    response = requests.get(url, params=params)
+    return Response(response.json())
+
+@api_view(['GET'])
+def get_bus_pos_by_veh_id(reqeust, veh_id):
+    url = 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByVehId'
+
+    params = {
+        'serviceKey': unquote('GMDNZxLlo35v0mYu1b%2BEExd5aIdZ93RCUBhUBo2w73LWCtz%2Ft%2F%2FKdGfzDVUdcyqljjwvNa5Dtd56uELhovFZRw%3D%3D'),
+        'vehId': veh_id,
+        'resultType': 'json'
+    }
+    response = requests.get(url, params=params)
+    return Response(response.json())
