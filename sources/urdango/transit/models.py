0357c740559e3e72e4b532d2ca22b1937bfc59cd
haneujeen <eujeenhan@gmail.com>
2023-07-02 00:37:50 +0900

    Update PushSubscription in models.py

--- sources/urdango/transit/models.py
+++ sources/urdango/transit/models.py
@@ -3,11 +3,8 @@ from django.db import models
 
 # Create your models here.
 class PushSubscription(models.Model):
-    endpoint = models.URLField(max_length=255, unique=True)
-    expiration_time = models.CharField(max_length=255, null=True)
-    p256dh = models.CharField(max_length=255)
-    auth = models.CharField(max_length=255)
+    subscription = models.JSONField()
 
     def __str__(self):
-        return f'PushSubscription {self.pk} for endpoint {self.endpoint}'
+        return f'PushSubscription {self.pk}: {self.subscription}'
 
