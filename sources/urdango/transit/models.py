3de54d644205f44683714bf3a0338766ee974fc1
haneujeen <eujeenhan@gmail.com>
2023-07-04 13:18:04 +0900

    Update `PushSubscription` model to include a foreign key to the `User` model

--- sources/urdango/transit/models.py
+++ sources/urdango/transit/models.py
@@ -3,12 +3,13 @@ import uuid
 
 
 # Create your models here.
+class User(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+
+
 class PushSubscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     subscription = models.JSONField()
 
     def __str__(self):
         return f'PushSubscription {self.pk}: {self.subscription}'
-
-
-class User(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
