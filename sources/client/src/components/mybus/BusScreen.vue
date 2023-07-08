0f2cd10ed05ffc2367e37bcd5029fb1cade5f7e1
haneujeen <eujeenhan@gmail.com>
2023-07-08 17:16:14 +0900

    Add 'mybus' directory under 'client/components' and update ui

--- sources/client/src/components/mybus/BusScreen.vue
+++ sources/client/src/components/mybus/BusScreen.vue
@@ -1,43 +1,44 @@
 <!-- BusScreen.vue -->
 <script setup>
-import { ref } from 'vue'
-
+/** Field description
+ * 'vehicle_id': Unique identifier for the bus
+ * 'line_name': The bus route or line name
+ * 'plate_number'
+ * 'travel_time': Time until the bus reaches The next immediate stop
+ * 'current_speed': Current traveling speed of the bus
+ * 'is_last_car': Whether there's no more buses scheduled after this bus
+ * 'previous_station' (id and name): Most recent stop the bus departed from
+ * 'station' (id and name): The next immediate stop bus is arriving
+ * 'next_station' (id and name): The proceeding bus stop
+ */
 const props = defineProps({
     bus: Object
 })
-
-const busData = ref({
-            rtNm: 'Bus 123',
-            plainNo: 'Plate ABC 123',
-            lastStnNm: 'Current Station',
-            this_station_name: 'Next Station',
-            next_station_name: 'Following Station'
-        })
 </script>
 
 <template>
-        <div class="card border-0 px-0 pb-0 text-muted shadow" style="background: linear-gradient(#f6f6f6 0%, #ffffff 100%); border-radius: 25px;">
+        <div class="card border-0 px-0 pb-0 text-muted shadow-sm" style="background: linear-gradient(#f6f6f6 0%, #fff 100%); border-radius: 25px;">
             <div class="card-body text-start mx-4 mb-3">
                 <div class="mb-3 stop">
                     <p class="m-0"><small>Current Stop</small></p>
-                    <h5>{{ busData.lastStnNm }}</h5>
+                    <h5>{{ bus.previous_station_name }}</h5>
                 </div>
                 <div class="mb-3 stop next">
                     <div class="row">
                         <div class="col-8">
                             <p class="m-0"><small>Current Stop</small></p>
-                            <h5>{{ busData.this_station_name }}</h5>
+                            <h5>{{ bus.station_name }}</h5>
                         </div>
                         <div class="col-4">badges</div>
                     </div>
                 </div>
                 <div class="mb-3 stop">
                     <p class="m-0"><small>Current Stop</small></p>
-                    <h5>{{ busData.next_station_name }}</h5>
+                    <h5>{{ bus.next_station_name }}</h5>
                 </div>
             </div>
             <div class="card-footer text-bg-danger border-0 text-start" style="border-radius: 0 0 25px 25px">
-                <p class="m-0"><small>{{ busData.rtNm }} {{ busData.plainNo }} Speed: km/h Estimated Time: 000</small></p>
+                <p class="m-0"><small>{{ bus.line_name }} {{ bus.plate_number }} Speed: {{ bus.current_speed }} km/h Time left to {{ bus.station_name }}: {{ bus.travel_time }}s</small></p>
             </div>
         </div>
 </template>
---
+++ sources/client/src/components/mybus/StopButton.vue
@@ -0,0 +1,63 @@
+<!-- StopButton.vue -->
+<script setup>
+import { ref, onMounted, nextTick } from 'vue'
+
+const stopRequested = ref(false)
+
+</script>
+
+<template>
+    <!-- This sends request to the bus's receiver module the user's on right now. -->
+    <p class="text-start pt-3"></p>
+    <div class="card border-0 px-0 pb-0 text-muted bg-light shadow-sm" style="border-radius: 25px;">
+        <div class="m-4 mb-3">
+            <button id="stopButton" class="btn btn-lg rounded-5 py-4" 
+                :class="[
+                    { 'btn-light': !stopRequested, 'btn-danger': stopRequested },
+                    { 'shadow': !stopRequested, 'shadow-sm': stopRequested }
+                ]"
+                :disabled="stopRequested"
+                style="border: 10px solid #ffffff;"
+                data-bs-toggle="modal" data-bs-target="#staticBackdrop"
+            >
+                <span class="h5">STOP</span>
+                <!-- TODO: add data for users with physical disadvantage -->
+                <!-- checkbox: Request additional aid when exiting -->
+            </button>
+        </div>
+        <div class="card-footer text-bg-light border-0 text-start" style="border-radius: 0 0 25px 25px">
+            <p class="m-0"><small>Device Name: My iPhone Requested User: Someone</small></p>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel"></h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    This immediately sends request to the bus's receiver module the user's on at the moment.
+                    A user cannot take back once the request is sent.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="stopRequested = true">Get me out</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* For web */
+#stopButton:hover {
+    background-color: #ce7474 !important; 
+}
+
+/* For mobile devices */
+#stopButton:active {
+    background-color: #c45e5e !important;
+}
+</style>
